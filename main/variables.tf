6bd5bccc0a93b8b4f0fd9033af1e041ee4ec13e5
ned1313 <ned.bellavance@gmail.com>
2022-01-20 12:04:20 -0500

    some refactoring

--- main/variables.tf
+++ main/variables.tf
@@ -3,29 +3,29 @@
 #############################################################################
 
 variable "location" {
+  description = "(Optional) Region where the Azure resources will be created. Defaults to East US."
   type    = string
   default = "eastus"
 }
 
 variable "naming_prefix" {
+  description = "(Optional) Naming prefix used for resources. Defaults to adolabs."
   type    = string
   default = "adolabs"
 }
 
 variable "asp_tier" {
+  description = "(Required) Tier for App Service Plan (Standard, PremiumV2)."
   type        = string
-  description = "Tier for App Service Plan (Standard, PremiumV2)"
-  default     = "Standard"
 }
 
 variable "asp_size" {
+  description = "(Required) Size for App Service Plan (S2, P1v2)."
   type        = string
-  description = "Size for App Service Plan (S2, P1v2)"
-  default     = "S1"
 }
 
 variable "capacity" {
+  description = "(Optional) Number of instances for App Service Plan."
   type        = string
-  description = "Number of instances for App Service Plan"
   default     = "1"
 }

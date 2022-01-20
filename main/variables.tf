4519b7e0e1dbb31cce224919e574cabbe3159788
ned1313 <ned.bellavance@gmail.com>
2022-01-20 14:04:36 -0500

    fix formatting

--- main/variables.tf
+++ main/variables.tf
@@ -4,14 +4,14 @@
 
 variable "location" {
   description = "(Optional) Region where the Azure resources will be created. Defaults to East US."
-  type    = string
-  default = "eastus"
+  type        = string
+  default     = "eastus"
 }
 
 variable "naming_prefix" {
   description = "(Optional) Naming prefix used for resources. Defaults to adolabs."
-  type    = string
-  default = "adolabs"
+  type        = string
+  default     = "adolabs"
 }
 
 variable "asp_tier" {

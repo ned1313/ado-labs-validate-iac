98156c9338c31ec3e6c6c6da8b073719fad46d74
ned1313 <ned.bellavance@gmail.com>
2022-08-04 15:30:21 -0400

    bad format

--- main/main.tf
+++ main/main.tf
@@ -25,9 +25,9 @@ resource "azurerm_resource_group" "app_service" {
 
 resource "azurerm_app_service_plan" "app_service" {
   name                = local.app_service_plan_name
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-  #resource_group_name = azurerm_resource_group.app_service.bad_attribute
+  location            =         azurerm_resource_group.app_service.location
+  #resource_group_name = azurerm_resource_group.app_service.name
+  resource_group_name = azurerm_resource_group.app_service.bad_attribute
 
   sku {
     tier     = var.asp_tier
